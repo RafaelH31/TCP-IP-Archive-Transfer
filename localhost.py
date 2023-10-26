45f7ac912910d61ceefa6eb04790c29e1cf8216e
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-10-26 11:42:05 -0300

    adcionando jpg e plain txt

--- localhost.py
+++ localhost.py
@@ -1,18 +1,29 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+print("insira o nome do arquivo e a extensão dele ")
+a = input()
 
 class Servidor(BaseHTTPRequestHandler):
 
     def do_GET(self):
-       if self.path == '/':
-           self.path = '/test.html'
-       try:
-           file_to_open = open(self.path[1:]).read()
-           self.send_response(200)
-       except:
-           file_to_open = "O aqrquivo não foi achado"
-           self.send_response(404)
-       self.end_headers()
-       self.wfile.write(bytes(file_to_open, 'utf-8'))
+        if self.path == '/':
+            self.path = '/{}'.format(a)
+        try:
+            if self.path.endswith('.html'):
+                content_type = 'text/html'
+            elif self.path.endswith('.jpg'):
+                content_type = 'image/jpeg'
+                #aqui vai ficar grande
+            else:
+                #basicamente código
+                content_type = 'text/plain'
+            file_to_open = open(self.path[1:], 'rb').read()
+            self.send_response(200)
+        except FileNotFoundError:
+            file_to_open = "O arquivo não foi encontrado"
+            self.send_response(404)
+        self.send_header('Content-type', content_type)
+        self.end_headers()
+        self.wfile.write(file_to_open)
 
-httpd = HTTPServer(('localhost',8080),Servidor)
+httpd = HTTPServer(('localhost', 8080), Servidor)
 httpd.serve_forever()
