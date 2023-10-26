e9e06e5b4c2798742403498dce83186262e30b78
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-10-26 14:36:02 -0300

    implementação da poc da importantissima e essencial de acordo com o user story das protos personas brudeny do carmo e Cesar juvencio.

--- download/url.py
+++ download/url.py
@@ -1,4 +1,13 @@
 from urllib import request
+from alive_progress import alive_bar
+import time
+
+def a():
+    for total in 5000, 7000, 4000, 0:
+        with alive_bar(total) as bar:
+            for _ in range(5000):
+                time.sleep(.001)
+                bar()
 
 print("insira url do quer baixar")
 url = input()
@@ -7,3 +16,7 @@ print("insira o nome e a extensão do arquivo")
 file = input()
 
 request.urlretrieve(url, file)
+value = True 
+while (value):
+    a()
+
