92dea088910da140bc9a84b1d92060b55ec53a70
Enzo Victor Stanganelli Gama <stanganelli899@gmail.com>
2023-10-29 17:54:53 -0300

    lógica de bat como aplicação

---
+++ main/app.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo Permitindo trafico para a porta 8080
+netsh advfirewall firewall add rule name="Permit Porta 8080" dir=in action=allow protocol=TCP localport=8080
+echo Regra Criada.
+
+echo pressione qualquer tecla para iniciar o progama
+pause > nul
+
+echo Iniciando o arquivo localhost.py
+python localhost.py
+echo O arquivo localhost.py foi executado com sucesso.
+
+echo agora que fechou o progama aperte qualquer tecla para fechar a porta 
+pause > nul
+
+@echo Excluindo regra que permite o tráfico
+netsh advfirewall firewall delete rule name="Permit Porta 8080"
+echo Regra excluída.
+
+pause 
